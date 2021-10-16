afb85d927c16ee87739ed10fde5c7f69cdbc6c71
Ggkenios <ggkenios@hotmail.com>
2021-10-16 20:23:47 +0200

    Confusion Matrix (#5)

--- plots.py
+++ plots.py
@@ -13,3 +13,18 @@ df_cm = pd.DataFrame(array,
                      columns=CLASS_LIST)
 plt.figure(figsize=(10, 7))
 sn.heatmap(df_cm, annot=True)
+
+'''
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.7, 1])
+plt.legend(loc='best')
+
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(loc='best')
+'''
