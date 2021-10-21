b8a0fc0246fafd7cc85179af9eca475961295bf7
Ggkenios <ggkenios@hotmail.com>
2021-10-21 21:57:01 +0200

    Comments (#10)

--- common/support_functions.py
+++ common/support_functions.py
@@ -1,4 +1,5 @@
 import numpy
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import models, losses
 from tensorflow.keras.layers import LSTM, Bidirectional, Dense, BatchNormalization
@@ -16,9 +17,9 @@ def model_build():
     # Model
     rnn_model = models.Sequential(
         [
-            LSTM(64, return_sequences=True, input_shape=(TIME_POINTS, NUMBER_OF_CHANNELS)),
-            LSTM(64, return_sequences=True),
-            LSTM(32),
+            LSTM(UNITS, return_sequences=True, input_shape=(TIME_POINTS, NUMBER_OF_CHANNELS)),
+            LSTM(UNITS, return_sequences=True),
+            LSTM(int(UNITS/2)),
             BatchNormalization(),
             Dense(OUTPUT_SIZE, activation='softmax'),
         ]
@@ -87,3 +88,43 @@ def majority_vote(all_predictions: list, dic: dict, k: int, i: int, y: numpy.nda
             dic["t2_p1"] += 1
         elif prediction == 2:
             dic["t2_p2"] += 1
+
+
+def train_test_set(x_array: numpy.array, y_array: numpy.array, dic: dict, *args: object) -> object:
+    """Get the train test split by having as input the data and patient IDs to include in the training set.
+
+    Args:
+        x_array (numpy.array): EEG data numpy array (3D)
+        y_array (numpy.array): Label data numpy array (1D)
+        dic (dict): Dictionary with count of 2-second batches per patient
+        *args (int): Patient IDs of the patients to include in the test set.
+
+    Returns:
+        x_train: Train split of our data
+        x_test: Test split of our data
+        y_train: Train split of label data
+        y_test: Test split of label data
+    """
+
+    # First, we create a dictionary
+    # Key: The starting data point of a specific patient
+    # Value: The final data point of the specific patient
+    dic_range = {}
+    for patient_id in args:
+        summation = 0
+        for previous_patient_ids in range(0, patient_id):
+            summation += dic[previous_patient_ids]
+        dic_range[summation] = summation + dic[patient_id]
+
+    # Then we create the a list with all the indexes for these patients
+    index_list = []
+    for k, v in dic_range.items():
+        index_list = index_list + list(range(k, v))
+
+    # Finally create a train-test data split.
+    x_test = x_array[index_list]
+    y_test = y_array[index_list]
+    x_train = np.delete(x_array, index_list, axis=0)
+    y_train = np.delete(y_array, index_list, axis=0)
+
+    return x_train, x_test, y_train, y_test
---
+++ split_by_patient.py
@@ -0,0 +1,52 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+
+from common import (
+    PATH,
+    OUTPUT_SIZE,
+    BATCH_SIZE,
+    EPOCHS,
+    checkpoint_acc,
+    lr_reducer,
+    model_build,
+    model_compile,
+    train_test_set,
+)
+
+x = np.load(f"{PATH}x_data.npy")
+y = np.load(f"{PATH}y_data.npy")
+z = np.load(f"{PATH}z_data.npy")
+
+unique, counts = np.unique(z, return_counts=True)
+patient_counts = dict(zip(unique, counts))
+
+# Run the function
+(x_train,
+ x_test,
+ y_train,
+ y_test,
+ ) = train_test_set(x, y, patient_counts, 8, 12, 15, 30, 40, 42, 44, 45, 47, 48)
+
+# To tensorflow tensors
+x_train = tf.convert_to_tensor(x_train)
+x_test = tf.convert_to_tensor(x_test)
+y_train = tf.convert_to_tensor(to_categorical(y_train, OUTPUT_SIZE))
+y_test = tf.convert_to_tensor(to_categorical(y_test, OUTPUT_SIZE))
+
+# To tensorflow dataset
+train = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(BATCH_SIZE)
+validation = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(BATCH_SIZE)
+
+# Build and compile the model
+model = model_build()
+model_compile(model)
+
+# Start training
+history = model.fit(
+    train,
+    validation_data=validation,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    callbacks=[checkpoint_acc, lr_reducer],
+)
