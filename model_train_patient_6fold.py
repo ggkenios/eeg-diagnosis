db9534641c71448720bd1a6dec1478075d9947f3
Ggkenios <ggkenios@hotmail.com>
2021-11-07 21:47:05 +0100

    Ready for testing

--- model_train_patient_6fold.py
+++ model_train_patient_6fold.py
@@ -1,40 +1,44 @@
 import numpy as np
 import pandas as pd
-from sklearn.utils.class_weight import compute_class_weight
 
 from common import (
     PATH,
     BATCH_SIZE,
     EPOCHS,
-    checkpoint_acc_2,
     lr_reducer,
     model_build,
     model_compile,
     train_test_set_split,
-    plot_curves,
     tensor_preparation,
 )
 
-col = ["Patient_out", "Val_accuracy", "Val_loss"]
-df = pd.DataFrame(columns=col)
 
 # Read the data
 x = np.load(f"{PATH}/x_data.npy")
 y = np.load(f"{PATH}/y_data.npy")
 z = np.load(f"{PATH}/z_data.npy")
 
+# Folds for Cross Validation
+FOLDS = 6
+
+# Report
+col = ["Patient", "Val_accuracy", "Val_loss"]
+df = pd.DataFrame(columns=col)
+
 unique, counts = np.unique(z, return_counts=True)
 patient_counts = dict(zip(unique, counts))
 
-for patient_out in range(54):
-    # Run the function from 1.3 Support Functions to split dataset
+for i in range(FOLDS):
+    patient_out = [j for j in range(len(unique)) if (j+i) % FOLDS == 0]
+
+    # Split dataset by patient (1.4 function)
     (x_train,
      x_test,
      y_train,
      y_test,
-     ) = train_test_set_split(x, y, patient_counts, patient_out)
+     ) = train_test_set_split(x, y, patient_counts, *patient_out)
 
-    # To tensorflow dataset
+    # Create a shuffled tensorflow dataset (1.4 function)
     train, validation = tensor_preparation(x_train, x_test, y_train, y_test)
 
     # Build and compile the model
@@ -50,20 +54,15 @@ for patient_out in range(54):
         callbacks=[lr_reducer],
     )
 
-    # Accuracy - loss plots
-    acc = history.history['val_accuracy'][-1]
-    loss = history.history['val_loss'][-1]
-
     dictionary = {
         "Patient_out": patient_out,
-        "Val_accuracy": acc,
-        "Val_loss": loss,
+        "Val_accuracy": history.history['val_accuracy'][-1],
+        "Val_loss": history.history['val_loss'][-1],
     }
 
     df = df.append(dictionary, ignore_index=True)
 
-    # Save the file on each iteration, so we can pick up from where we left, if we stop the code.
-    df.to_csv(PATH + "/report_out.csv", index=False)
+    # Save every update, to keep the process from where it stopped
+    df.to_csv(PATH + "/report_6fold.csv", index=False)
 
-df.to_csv(PATH + "/report_out_f.csv", index=False)
-
+df.to_csv(PATH + "/report_6fold_f.csv", index=False)
