1f009850c0c602cf193be5b9ff6a3edcf4bc735b
George Gkenios <83859471+ggkenios@users.noreply.github.com>
2022-01-08 19:45:40 +0100

    Update split_45_9.py

--- split_by_patient/split_45_9.py
+++ split_by_patient/split_45_9.py
@@ -20,19 +20,18 @@ z = np.load(f"{PATH}/z_data.npy")
 
 patient_out = [2, 10, 15, 20, 25, 30, 37, 42, 51]
 
-# Split dataset by patient
-(
-    x_train,
-    x_test,
-    y_train,
-    y_test,
+# Split dataset by patient (2.2 function)
+(x_train,
+  x_test,
+  y_train,
+  y_test,
   ) = train_test_patient_split(x, y, z, *patient_out)
 
-# Create a shuffled tensorflow dataset
+# Create a shuffled tensorflow dataset (2.2 function)
 train, validation = tensor_preparation(x_train, x_test, y_train, y_test)
 
 # Build and compile the model
-model = model_build()
+model = locals()[MODEL]()
 model_compile(model)
 
 # Start training
@@ -43,3 +42,7 @@ history = model.fit(
     epochs=EPOCHS,
     callbacks=[checkpoints("p"), lr_reducer],
 )
+
+# Create confusion matrix
+model.load_weights(f"{PATH_CHECKPOINTS}/p_-1.h5")
+voting(model, x, y, z, *patient_out)
